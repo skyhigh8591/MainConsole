850d4a1d0bcb765a1cd7a2d32bee8cc44a4ab63f
Martin <taes891@gmail.com>
2022-04-05 14:46:39 +0800

    Initial commit

---
+++ MainConsole/Console.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainConsole
+{
+    public partial class Console : Form
+    {
+        public Console()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MainConsole
+ 
